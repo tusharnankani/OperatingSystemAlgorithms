3b6ed3381fbbda9f8f78b28ed386c78c9bf93ba0
tusharnankani <tusharnankani3@gmail.com>
2021-03-12 20:53:41 +0530

    :fire: feat: refactor code for tabular data (#1)

--- MemoryAllocation.java
+++ MemoryAllocation.java
@@ -6,19 +6,19 @@ class MemoryBlock {
     boolean[] free = new boolean[] { false, true, false, true, false, true, false, true };
     int[] processNumber = new int[] { 1, 2, 3, 4 };
     int divs = memory.length;
-    int processSize; 
+    int processSize;
     Scanner s = new Scanner(System.in);
     
     void processInput() {
-        System.out.println("Current Scenario of the Memory Allocation");
+        System.out.println("\n\tCurrent Scenario of the Memory Allocation \n");
         printTable(-1);
         System.out.print("Enter the size of the process that needs to be added (in KB): ");
         processSize = s.nextInt();
-        System.out.println("After First Fit \n");
+        System.out.println("\n\t\tAfter First Fit \n");
         firstFit();
-        System.out.println("After Best Fit  \n");
+        System.out.println("\n\t\tAfter Best Fit  \n");
         bestFit();
-        System.out.println("After Worst Fit \n");
+        System.out.println("\n\t\tAfter Worst Fit \n");
         worstFit();
     }
 
@@ -60,21 +60,24 @@ class MemoryBlock {
     }
     
     void printTable(int pos) {
-        System.out.print("No. \tMemory \tStatus \tProcess \n");
+        System.out.print("+----------------------------------------------------------+\n");
+        System.out.print("|\tNo.\tMemory \t\t Status \t Process   |\n");
+        System.out.print("+----------------------------------------------------------+\n");
         int j = 1, ok = 0;
         for (int i = 0; i < divs; i++) {
             if(i == pos) {
-                System.out.print(i + 1 + " \t " +  processSize + " \t " + "NF \t " + "Process " + (processNumber.length + 1) + "\n");
+                System.out.print("|\t" + (i + 1) + " \t " +  processSize + "  \t\t " + " NF \t\t " + "Process " + (processNumber.length + 1) + " |");
                 if(memory[i] - processSize != 0) {
-                    System.out.print(i + 2 + " \t " + (memory[i] - processSize) + " \t " + "F \t ");
+                    System.out.print("\n|\t" + (i + 2) + " \t " + (memory[i] - processSize) + "  \t\t " + " F \t\t\t   |");
                     ok = 1;
                 }
             }
             else {
-                System.out.print((i + 1 + ok) + " \t " +  memory[i] + " \t " + ((free[i]) ? "F" : "NF \t " + "Process " + j++));
+                System.out.print("|\t" + (i + 1 + ok) + " \t " +  memory[i] + "  \t\t  " + ((free[i]) ? "F \t\t\t   |" : "NF \t\t " + "Process " + j++ + " |"));
             }
             System.out.println(' ');
         }
+        System.out.print("+----------------------------------------------------------+\n");
     }
 }
 
